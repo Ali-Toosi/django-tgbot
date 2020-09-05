89e7b57727925c76ac1df6ee78815813ed1f1600
Purwo Widodo <id.purwowd@gmail.com>
2020-09-05 07:11:57 +0700

    removed sampleproject

--- sampleproject/botdohkali_bot/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import credentials
-
-
-bot_token = credentials.BOT_TOKEN
-app_name = credentials.APP_NAME
--- sampleproject/botdohkali_bot/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import handle_bot_request
-
-urlpatterns = [
-    path('update/', handle_bot_request)
-]
